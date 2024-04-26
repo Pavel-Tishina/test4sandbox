8f20757475a49686f5e465b3d6bb0e1bf126b60a
Pavel Tsikota <pavel.tsikota@softacus.com>
2024-04-26 09:21:26 +0200

    -add int params

--- src/main/java/com/itgnostic/test4sandbox/utils/RestApiUtils.java
+++ src/main/java/com/itgnostic/test4sandbox/utils/RestApiUtils.java
@@ -2,11 +2,32 @@ package com.itgnostic.test4sandbox.utils;
 
 import org.h2.util.StringUtils;
 
+import java.util.stream.Stream;
+
 public class RestApiUtils {
-    private static final String REGEX_PARAMS = "(\\w,?)+";
-    public static String[] getParamsAsArray(String paramValue) {
+    private static final String REGEX_PARAMS = "(\\w+,?)+";
+    private static final String REGEX_INT_PARAMS = "(\\d+,?)+";
+    public static String[] getParamsAsArray(String paramValue, boolean onlyUnique) {
         return !StringUtils.isNullOrEmpty(paramValue) && paramValue.matches(REGEX_PARAMS)
-                ? paramValue.split(",")
+                ? split2String(paramValue, onlyUnique)
                 : new String[0];
+    }
+
+    public static int[] getIntParamsAsArray(String paramValue, boolean onlyUnique) {
+        return !StringUtils.isNullOrEmpty(paramValue) && paramValue.matches(REGEX_INT_PARAMS)
+                ? split2Int(paramValue, onlyUnique)
+                : new int[0];
+    }
+
+    private static String[] split2String(String paramValue, boolean onlyUnique) {
+        return onlyUnique
+                ? Stream.of(paramValue.split(",")).distinct().toArray(String[]::new)
+                : paramValue.split(",");
+    }
+
+    private static int[] split2Int(String paramValue, boolean onlyUnique) {
+        return onlyUnique
+                ? Stream.of(paramValue.split(",")).mapToInt(Integer::parseInt).distinct().toArray()
+                : Stream.of(paramValue.split(",")).mapToInt(Integer::parseInt).toArray();
     }
 }
--- src/test/java/com/itgnostic/test4sandbox/utils/RestApiUtilsTest.java
+++ src/test/java/com/itgnostic/test4sandbox/utils/RestApiUtilsTest.java
@@ -1,21 +1,22 @@
 package com.itgnostic.test4sandbox.utils;
 
-import org.h2.util.StringUtils;
 import org.junit.Test;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 public class RestApiUtilsTest {
-    private static final String PARAMS_BAD_1 = null;
-    private static final String PARAMS_BAD_2 = "";
-    private static final String PARAMS_BAD_3 = "&&&";
-    private static final String PARAMS_BAD_4 = "one ";
-    private static final String PARAMS_BAD_5 = "\t5";
-    private static final String PARAMS_BAD_6 = "5, one";
-    private static final String PARAMS_BAD_7 = "5,one&";
+    private static final String[] PARAMS_BAD = {null, "", "&&&", "one ", "\t5", "5, one", "5,one&"};
     private static final String PARAMS_OK_1 = "one";
     private static final String PARAMS_OK_2 = "5";
     private static final String PARAMS_OK_3 = "5,one";
+    private static final String PARAMS_OK_4 = "5,one,one";
+
+    private static final String[] PARAMS_INT_BAD = {null, "", "-5", " 5", "5.2", "5o", "5, 5", "5,s", "5&", "5,5?", "5,-5"};
+    private static final String PARAMS_INT_OK_1 = "5";
+    private static final String PARAMS_INT_OK_2 = "5,5,5";
+    private static final String PARAMS_INT_OK_3 = "5,6";
+    private static final String PARAMS_INT_OK_4 = "5,6,5";
 
 
     @Test
@@ -24,17 +25,35 @@ public class RestApiUtilsTest {
         String[] oneValue = {"one"};
         String[] oneValue2 = {"5"};
         String[] lotValues = {"5","one"};
+        String[] lotValuesNotUniq = {"5","one","one"};
 
-        assertEquals(badValue, RestApiUtils.getParamsAsArray(PARAMS_BAD_1));
-        assertEquals(badValue, RestApiUtils.getParamsAsArray(PARAMS_BAD_2));
-        assertEquals(badValue, RestApiUtils.getParamsAsArray(PARAMS_BAD_3));
-        assertEquals(badValue, RestApiUtils.getParamsAsArray(PARAMS_BAD_4));
-        assertEquals(badValue, RestApiUtils.getParamsAsArray(PARAMS_BAD_5));
-        assertEquals(badValue, RestApiUtils.getParamsAsArray(PARAMS_BAD_6));
-        assertEquals(badValue, RestApiUtils.getParamsAsArray(PARAMS_BAD_7));
+        for (String badParam : PARAMS_BAD)
+            assertArrayEquals(badValue, RestApiUtils.getParamsAsArray(badParam, true));
 
-        assertEquals(oneValue, RestApiUtils.getParamsAsArray(PARAMS_OK_1));
-        assertEquals(oneValue2, RestApiUtils.getParamsAsArray(PARAMS_OK_2));
-        assertEquals(lotValues, RestApiUtils.getParamsAsArray(PARAMS_OK_3));
+        assertArrayEquals(oneValue, RestApiUtils.getParamsAsArray(PARAMS_OK_1, true));
+        assertArrayEquals(oneValue2, RestApiUtils.getParamsAsArray(PARAMS_OK_2, true));
+        assertArrayEquals(lotValues, RestApiUtils.getParamsAsArray(PARAMS_OK_3, true));
+        assertArrayEquals(lotValues, RestApiUtils.getParamsAsArray(PARAMS_OK_4, true));
+        assertArrayEquals(lotValuesNotUniq, RestApiUtils.getParamsAsArray(PARAMS_OK_4, false));
+    }
+
+    @Test
+    public void getIntParamsAsArrayTest() {
+        int[] badValue = new int[0];
+        int[] oneValue = {5};
+        int[] lotValues = {5,6};
+        int[] lotValuesNotUniq1 = {5,5,5};
+        int[] lotValuesNotUniq2 = {5,6,5};
+
+        for (String badParam : PARAMS_INT_BAD)
+            assertArrayEquals(badValue, RestApiUtils.getIntParamsAsArray(badParam, true));
+
+        assertArrayEquals(oneValue, RestApiUtils.getIntParamsAsArray(PARAMS_INT_OK_1, true));
+        assertArrayEquals(oneValue, RestApiUtils.getIntParamsAsArray(PARAMS_INT_OK_2, true));
+        assertArrayEquals(lotValues, RestApiUtils.getIntParamsAsArray(PARAMS_INT_OK_3, true));
+        assertArrayEquals(lotValues, RestApiUtils.getIntParamsAsArray(PARAMS_INT_OK_4, true));
+
+        assertArrayEquals(lotValuesNotUniq1, RestApiUtils.getIntParamsAsArray(PARAMS_INT_OK_2, false));
+        assertArrayEquals(lotValuesNotUniq2, RestApiUtils.getIntParamsAsArray(PARAMS_INT_OK_4, false));
     }
 }
