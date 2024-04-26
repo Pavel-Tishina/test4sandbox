c748bbee921ffe178553b342284d8b84097a5556
Pavel Tsikota <pavel.tsikota@softacus.com>
2024-04-26 06:03:28 +0200

    init

---
+++ src/main/java/com/itgnostic/test4gamebox/Test4gameboxApplication.java
@@ -0,0 +1,13 @@
+package com.itgnostic.test4gamebox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test4gameboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test4gameboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itgnostic/test4gamebox/Test4gameboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itgnostic.test4gamebox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test4gameboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
