c91bc836bd2eb02d48ebca71c002e5d6c05dde37
Pavel Tsikota <pavel.tsikota@softacus.com>
2024-04-28 14:56:18 +0200

    - add entity

--- src/test/java/com/itgnostic/test4sandbox/db/entity/EmployeeEntityTest.java
+++ src/test/java/com/itgnostic/test4sandbox/db/entity/EmployeeEntityTest.java
@@ -1,9 +1,11 @@
 package com.itgnostic.test4sandbox.db.entity;
 
+import jakarta.persistence.criteria.CriteriaBuilder;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
-import org.junit.jupiter.api.Order;
 import org.junit.runners.MethodSorters;
+
+import java.util.*;
 
 import static org.junit.Assert.*;
 
@@ -34,5 +36,11 @@ public class EmployeeEntityTest {
 
         e2.setPosition("");
         assertFalse(e1.equals(e2));
+
+        Set<Integer> d1 = Set.of(6, 1, 3, 4);
+        Collection<Integer> d2 = Arrays.asList(6, 1, 3, 4);
+
+
+        System.out.println(Objects.deepEquals(d1, d2));
     }
 }
